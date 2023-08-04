dc81ed54fec1408e7166b3d7672f845b8c59aa8a
Sloompop <141396884+Sloompop@users.noreply.github.com>
2023-08-04 04:17:04 -0300

    NBLYYH

---
+++ funs.js
@@ -0,0 +1,7 @@
+ function fun(){
+
+              
+              if (document.getElementById('arch').value=="THEBATTLEOFIDENTITY"){
+                  window.location.href = "https://drive.google.com/file/d/1Nih1_61dhFAv-wSoiO146qCraGtjm3Zy/view?usp=sharing";
+              }
+          }
